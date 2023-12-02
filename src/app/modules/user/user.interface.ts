885a0ffc8b74c2a056b86bcc7106a84012d8fe90
SUMON <belalhossain22000@gmail.comgit config --global user.name>
2023-12-03 01:21:28 +0600

    added user interface

--- src/app/modules/user/user.interface.ts
+++ src/app/modules/user/user.interface.ts
@@ -1,8 +1,10 @@
+
 export type TUser = {
-  id: string;
-  password: string;
-  needsPasswordChange: boolean;
-  role: 'admin' | 'student' | 'faculty';
-  status: 'in-progress' | 'blocked';
-  isDeleted: boolean;
-};
+    id: string;
+    password: string;
+    needPasswordChange: boolean;
+    role: 'admin' | 'student' | 'faculty';
+    status: 'block' | 'in-progress';
+    isDeleted: boolean
+
+}
